87df74de1ae89569741a9d32023d2fd8347e966d
Aaron Ervin <aervin@ixsystems.com>
2019-08-11 09:38:06 -0500

    adds source code tab to demo examples

--- projects/hammerhead-ui-app/src/app/components/shared/shared-components.module.ts
+++ projects/hammerhead-ui-app/src/app/components/shared/shared-components.module.ts
@@ -10,11 +10,13 @@ import {
     MatListModule,
     MatSidenavModule,
     MatSnackBarModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule
 } from '@angular/material';
 import { HammerheadUiModule } from 'hammerhead-ui';
 import { DemoExampleComponent } from './demo-example/demo-example.component';
 import { DemoComponent } from './demo/demo.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 const COMPONENTS = [DemoComponent, DemoExampleComponent];
 
@@ -27,12 +29,13 @@ const MATERIAL_MODULES = [
     MatListModule,
     MatSidenavModule,
     MatSnackBarModule,
+    MatTabsModule,
     MatToolbarModule
 ];
 
 @NgModule({
     declarations: COMPONENTS,
-    imports: [CommonModule, FlexLayoutModule, HammerheadUiModule, ...MATERIAL_MODULES],
+    imports: [CommonModule, FlexLayoutModule, HammerheadUiModule, MarkdownModule.forChild(), ...MATERIAL_MODULES],
     exports: [FlexLayoutModule, HammerheadUiModule, ...COMPONENTS, ...MATERIAL_MODULES]
 })
 export class SharedComponentsModule {}
---
+++ projects/hammerhead-ui-app/src/app/views/components/components/zero-state-demo/examples/zero-state-example-1/zero-state-example-1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-zero-state-example-1',
+    template: `
+        <hh-zero-state class="example" message="No data available"></hh-zero-state>
+    `
+})
+export class ZeroStateExample1Component {}
