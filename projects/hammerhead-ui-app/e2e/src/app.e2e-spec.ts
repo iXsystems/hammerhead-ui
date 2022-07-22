f60c1de7a5082840d8f8738e82d7cf17f8534056
Gerald Miral <gerald.miral@gmail.com>
2022-07-23 02:13:05 +0800

    TC-2171 #in-progress setup storybook

--- projects/hammerhead-ui-app/e2e/src/app.e2e-spec.ts
+++ projects/hammerhead-ui-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to hammerhead-ui-app!');
+    expect<any>(page.getTitleText()).toEqual('Welcome to hammerhead-ui-app!');
   });
 
   afterEach(async () => {
